e88db9770592e7045e008a9f71eb6e7d5185433a
Sheena Sang <sheenasany@gmail.com>
2022-09-15 22:50:30 -0400

    added new font

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -8,6 +8,9 @@ function App() {
 
   return (
     <div className = "app">
+      <style>
+      @import url('https://fonts.googleapis.com/css2?family=UnifrakturMaguntia&display=swap');
+      </style>
       <h1>Adventure Character Generator</h1>
       <h2>Welcome Players!</h2>
       <p></p>
