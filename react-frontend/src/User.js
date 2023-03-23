8069b610e73e060e41d0f57ab5e0377c70ed0c2f
Sheena Sang <sheenasany@gmail.com>
2023-03-23 10:36:13 -0400

    added notes and cleaned up code

--- react-frontend/src/User.js
+++ react-frontend/src/User.js
@@ -23,23 +23,19 @@ function User({ users, handleUsers }){
         fetchingUserCharacters(e.target.value)  
     }
 
-    const fetchingUserCharacters = (user) => {axios.get(`http://localhost:9292/users/${user}/characters`).then(resp => setCharacters(resp.data))}
-
-    function handleClick(e){
-       if(e.target.value === "creation"){
-        setUserRoute(true)
-       }
-       else {
-        setUserRoute(false)
-       }
+    const fetchingUserCharacters = (user) => {
+        axios.get(`http://localhost:9292/users/${user}/characters`).then(resp => setCharacters(resp.data))
     }
 
-    // function handleSelectCharClick () {
-    //     setSelectCharBtn()
-    // }
-
-    // function handleCreateCharClick () {
-    // }
+    // added a value to each button to register the event and reset the value of userRoute on a click
+    const handleClick = (e) => {
+    if(e.target.value === "creation"){
+        setUserRoute(true)
+    }
+    else{
+        setUserRoute(false)
+        }
+    }
 
     const handleUserName = (e) => {
         setUserName(e.target.value)
@@ -47,12 +43,14 @@ function User({ users, handleUsers }){
 
     let userList = users.map(user => <option value={user.id} key={user.id}>{user.name}</option>)
 
-    function handleRendering(){
+    // handles the rendering of each component depending on the state of userRoute 
+    // && whether there is a user clicked or whether a character is fetched properly
+    const handleRendering = () => {
         if(userRoute === true && user !== ""){
-          return <CharacterCreation user={user}/>
+            return <CharacterCreation user={user}/>
         }
-        if(userRoute=== false && characters !== null){
-           return <CharacterList user={user} characters={characters}/>
+        if(userRoute === false && characters !== null){
+            return <CharacterList user={user} characters={characters}/>
         }
     }
 
@@ -66,21 +64,20 @@ function User({ users, handleUsers }){
 
         {userRoute ? <h2>Hark! Create Thy Champion!</h2>: <h2>Adventurer! Choose Your Character!</h2>}
         {userRoute ? <h5>Select your username. Enter a character name and background, and your character will be auto-generated!</h5>: <h5>View your created characters in all their resplendent glory with stats. Click on your character to delete or update them.</h5> }
-           {/* <button class="fancy-btn" onClick={handleClick}>{userRoute ?  "Switch to Character Selection" : "Change to Character Creation"}</button> */}
         <button class="fancy-btn" value="creation" onClick={handleClick}>Character Creation</button>
         <button class="fancy-btn" value="selection" onClick={handleClick}>Character Selection</button>
         <br/>
 
-           <select onChange={handleChange}>
-            <br/>
-           <option>Select User</option>
-           {userList}
-           </select>
-           {/* {userRoute ? user !== "" ? <CharacterCreation user={user}/> : null : characters === null ? null : <CharacterList user={user} characters={characters}/>} */}
+        <select onChange={handleChange}>
+        <br/>
+        <option>Select User</option>
+            {userList}
+        </select>
+            {/* Leaving this here for you to say goodbye, Quade..*/}
+            {/* {userRoute ? user !== "" ? <CharacterCreation user={user}/> : null : characters === null ? null : <CharacterList user={user} characters={characters}/>} */}
         {handleRendering()} 
         </div>
     )
 }
-
 
 export default User;
