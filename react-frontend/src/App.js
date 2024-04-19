4a2478771f439cfc7a1bad15ec25da85d2473d0a
Quade Houston <quadehouston@gmail.com>
2024-04-18 22:00:42 -0400

    add sessions and refactoring

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,20 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Route, Routes, Navigate, useNavigate } from "react-router-dom";
 import './App.css';
 import Home from "./Home"
+import Login from "./Login"
+import CharacterCreation from "./CharacterCreation";
+import CharacterList from "./CharacterList";
+import CharacterCard from "./CharacterCard";
+import axios from "axios";
 
 function App() {
+  const [user, setUser] = useState(() => {
+    const newUser = localStorage.getItem('loadedUser')
+    return newUser ? JSON.parse(newUser) : null
+  })
+  const [characters, setCharacters] = useState(() => {
+    const newCharacters = localStorage.getItem('loadedCharacters')
+    return newCharacters ? JSON.parse(newCharacters) : []
+  })
+  const navigate = useNavigate()
   
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/me");
+          localStorage.setItem('loadedCharacters', JSON.stringify(response.data.characters));
+          localStorage.setItem('loadedUser', JSON.stringify(response.data));
+      } catch (error) {
+        console.error('Authentication error:', error);
+      }
+    };
 
+    fetchData();
+  }, []);
 
+  const handleUser = (userData) => {
+    setUser(userData)
+    setCharacters(userData.characters)
+    localStorage.setItem('loadedUser', JSON.stringify(userData))
+    localStorage.setItem('loadedCharacters', JSON.stringify(userData.characters));
+  }
+
+  const handleCharacters = (characterData) => {
+    setCharacters(characterData)
+  }
+  
+  const handleSelectedCharacter = (character) => {
+      localStorage.removeItem('selectedCharacter')
+      navigate('/character_card', {state: {character: character}})
+  }
+
+  const handleLogout = () => {
+    axios.delete('/logout')
+    setUser(null)
+    localStorage.clear()
+    navigate('/login')
+}
+  
   return (
     <div className = "app">
       <style>
       @import url('https://fonts.googleapis.com/css2?family=UnifrakturMaguntia&display=swap');
       </style>
       <h1>Adventure Character Generator</h1>
-      <h2>Welcome Players!</h2>
-      <p></p>
-      <Home/>
+      <Routes>
+        <Route path="/" element={user ? <Home handleUser={handleUser} handleLogout={handleLogout}/> : <Navigate replace to="/login" />} />
+        <Route path="/login" element={!user ? <Login handleUser={handleUser} /> : <Navigate replace to="/" />} />
+        {user ? <Route path="character_creation" element={<CharacterCreation userId={user.id} characters={characters} handleCharacters={handleCharacters}/>} /> : null}
+        {user ? <Route path="character_selection" element={<CharacterList characters={characters} handleSelectedCharacter={handleSelectedCharacter} />} /> : null}
+        <Route path="character_card" element={<CharacterCard characters={characters} handleCharacters={handleCharacters}/>} />
+      </Routes>
     </div>
   );
 }
--- react-frontend/src/Home.js
+++ react-frontend/src/Home.js
@@ -1,23 +1,23 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import User from './User'
+import { useNavigate } from 'react-router-dom'
 
-function Home() {
-    const [users, setUsers] = useState([])
+function Home({ handleLogout }) {
+    const navigate = useNavigate()
   
-    useEffect( () => {
-    axios.get("http://localhost:9292/users").then(resp => setUsers(resp.data))
-    }, [])
-    
-   const handleUsers = (users) => {
-    setUsers(users)
-   }
+    const handleClick = (e) => {
+        if(e.target.value === "creation"){
+            navigate('character_creation')
+        }
+        else if (e.target.value === "selection"){
+            navigate('/character_selection')
+            }
+        }
 
     return(
         <div>
             <h5>This handy app will help you create and keep track of your original Roleplaying Characters.</h5>
-            <h5>Please create a username or select your username below...</h5>
-            <User users={users} handleUsers={handleUsers}/>
+            <button class="fancy-btn" value="creation" onClick={handleClick}>Character Creation</button>
+            <button class="fancy-btn" value="selection" onClick={handleClick}>Character Selection</button>
+            <button class="fancy-btn" value="log out" onClick={() => handleLogout()}>Log Out</button>
         </div>
     )
 }
